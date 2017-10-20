9474f7bb55eaa10e1bfb7f60b062467cdce1e833
Maryussuperboy <martsyalis@gmail.com>
2017-10-20 10:43:06 -0700

    studio post test passes

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -13,10 +13,26 @@ describe('Studios API', () => {
     });
 
     it('should save with id', () => {
-        request.post('/api/studios')
+        return request.post('/api/studios')
             .send(studio)
             .then( ({ body }) => {
                 assert.equal(body.name, studio.name);
+            });
+    });
+
+    it('Should post and then get studio', () => {
+        let saved = null;
+        return request.post('/api/studios')
+            .send(studio)
+            .then( ({body}) => {
+                console.log(' we are in test and body is:',body);
+                saved = body;
+                return request.get(`/api/studios/${saved._id}`);
+            })
+            .then ( ({ body }) =>{
+                console.log('saved is:', saved);
+                console.log('body is:', body);
+                assert.deepEqual(saved, body);
             });
     });
 
