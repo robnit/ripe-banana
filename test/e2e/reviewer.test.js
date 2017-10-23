74aca9e62d11f7c3a10b592c0ad23811b1973aef
Maryussuperboy <martsyalis@gmail.com>
2017-10-23 16:48:40 -0700

    working through reviewer get by id

--- test/e2e/reviewer.test.js
+++ test/e2e/reviewer.test.js
@@ -2,7 +2,8 @@ const { assert } = require('chai');
 const mongoose = require('mongoose');
 const request = require('./request');
 
-describe.skip('Reviewers API', () => {
+
+describe.only('Reviewers API', () => {
 
 
     const reviewerOne = {
@@ -19,6 +20,72 @@ describe.skip('Reviewers API', () => {
         mongoose.connection.dropDatabase();
     });
 
+    // check if we get his reviews when requesting reviewer by id
+    // post a reviewer.
+    // post a review. 
+    // post a film
+    // post a studio
+    // post an actor
+
+    let reviewer = null;
+    beforeEach ( () => {
+        return request.post('/api/reviewers')
+            .send(reviewerOne)
+            .then(({ body }) => reviewer = body);
+    });
+    
+
+    function saveStudio(studio){
+        return request.post('/api/studios')
+            .send(studio);
+    }
+
+    function saveActor(actor) {
+        return request.post('/api/actors')
+            .send(actor);
+    }
+
+    let actor = null;
+    beforeEach( () => {
+        return saveActor ( { name: 'Mel Gibson', pob:'My Basement'})
+            .then( ({body}) => actor = body);
+    });
+
+    let studio = null;
+    beforeEach( () => {
+        return saveStudio( { name: 'Universal'})
+            .then( ({body}) => studio = body);
+    });
+
+    let film = null;
+    beforeEach( () => {
+        return request.post('/api/films')
+            .send({
+                title: 'The Room',
+                studio: studio._id,
+                released: 2000,
+                cast: {
+                    actor: actor._id 
+                }
+            })
+            .then (({body}) =>{
+                film = body ;
+            });
+
+    });
+    let review = null;
+    beforeEach( () => {
+        request.post('api/reviews/')
+            .send({
+                rating: 4,
+                reviewer: reviewer._id,
+                review: 'Awsome movie',
+                film: film._id
+            })
+            .then (saved => review = saved);
+    });
+
+    
 
     it('should save with id', () => {
         return request.post('/api/reviewers')
@@ -41,17 +108,12 @@ describe.skip('Reviewers API', () => {
     });
 
 
-    it('should get reviewer by id, returning name, company, and reviews [film.name, rating, review]', ()=> {
-        return request.post('/api/reviewers')
-            .send(reviewerOne)
-            .then( ({ body }) => {
-                return request.get(`/api/reviewers/${body._id}`);
-            })
+    it.only('should get reviewer by id, returning name, company, and reviews [film.name, rating, review]', ()=> {
+        return request.get(`/api/reviewers/${reviewer._id}`)
             .then( ({body}) => {
-                assert.equal(body.name, reviewerOne.name);
-                // assert.equal(body.reviews.rating, reviewOne.rating);
-                // assert.equal(body.reviews.review, reviewOne.review);
-
+                assert.equal(body.name, reviewer.name);
+                assert.equal(body.reviews.rating, review.rating);
+                assert.equal(body.reviews.review, review.review);
             });
     });
 
